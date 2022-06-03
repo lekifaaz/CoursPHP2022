1ad8a25f25a382a6b7ded7f75fa7c6bd842f70d2
lekifaaz <faaki.tchabi@colombbus.org>
2022-06-03 10:03:45 +0200

    New version

---
+++ 02_variables/variables.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- font google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet">
+
+    <title>Cours_php2022 - Introduction</title>
+    <!-- Mes styles -->
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+    <div class="jumbotron bg-secondary text-center">
+        <h1 class="display-3 pt-3">Cours_php2022</h1>
+        <p class="lead pb-3">PHP signifie aujourd'hui Hypertext Preprocessor</p>
+    </div>
+
+    <div class="row">
+
+        <?php
+        require('../inc/sidenav.inc.php'); //ici on appelle le fichier sidenav.inc.php
+        ?>
+     
+        <!-- ========================================================= -->
+        <!-- Contenu principal -->
+        <!-- ========================================================= -->
+        
+        
+        <!-- fin de la partie principale, col-8 -->
+       
+    
+
+
+
+    </div>
+
+    <?php
+    require('../inc/footer.inc.php'); //ici on appelle le fichier footer.inc.php
+    ?>
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+    <!-- mon script js pour la navigation -->
+    <script src="../inc/sidenav.js"></script>
+</body>
+
+</html>
---
+++ inc/footer.inc.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3 bg-secondary">
+  <div class="container">
+    <p class="text-dark">Cours PHP_7 - &copy <?php echo date('Y') ?></p>
+  </div>
+</footer>
