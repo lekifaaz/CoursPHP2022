1eec9d06b0427dff48891e83c9cc24f80f23fdfa
Lekifaaz <faaki.tchabi@colombbus.org>
2022-06-17 15:14:54 +0200

    update

--- 08_securite/02-employes.php
+++ 08_securite/02-employes.php
@@ -4,12 +4,12 @@ require_once('../inc/functions.php');
 $host = 'localhost';
 $database = 'entreprise';
 $user = 'root';
-$psw = ''; // (root sur mac)
+$psw = '';
 
-$pdoENT = new PDO('mysql:host='.$host.';dbname='.$database,$user,$psw);
+$pdoENT = new PDO('mysql:host=' . $host . ';dbname=' . $database, $user, $psw);
 $pdoENT->exec("SET NAMES utf8");
 
-if ( !empty($_POST) ){
+if (!empty($_POST)) {
     //pour se prémunir des failles nous faisons ceci
     $_POST['prenom'] = htmlspecialchars($_POST['prenom']);
     $_POST['nom'] = htmlspecialchars($_POST['nom']);
@@ -18,8 +18,11 @@ if ( !empty($_POST) ){
     $_POST['date_embauche'] = htmlspecialchars($_POST['date_embauche']);
     $_POST['salaire'] = htmlspecialchars($_POST['salaire']);
 
-    $requete = $pdoENT->prepare("INSERT INTO employes(prenom, nom, sexe, service, date_embauche, salaire) VALUES (:prenom, :nom, :sexe, :service, :date_embauche, :salaire)");
-    //NOW();
+    //$requete = $pdoENT->prepare( " INSERT INTO employes (prenom, nom, sexe, service, date_embauche, salaire) VALUES (:prenom, :nom, :sexe, :service, NOW(), :salaire) " );
+    //NOW() renvoie la date d'aujourd'hui
+
+    $requete = $pdoENT->prepare(" INSERT INTO employes (prenom, nom, sexe, service, date_embauche, salaire) VALUES (:prenom, :nom, :sexe, :service, :date_embauche, :salaire) ");
+
     $requete->execute(array(
         ':prenom' => $_POST['prenom'],
         ':nom' => $_POST['nom'],
@@ -27,10 +30,8 @@ if ( !empty($_POST) ){
         ':service' => $_POST['service'],
         ':date_embauche' => $_POST['date_embauche'],
         ':salaire' => $_POST['salaire'],
-
     ));
-}// fin if!empty
-
+} //fin if !empty
 ?>
 <!doctype html>
 <html lang="fr">
@@ -47,7 +48,7 @@ if ( !empty($_POST) ){
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet">
 
-    <title>Cours PHP2022 - Entreprise et employes</title>
+    <title>Cours PHP7 - Entreprise et employés</title>
 
     <!-- mes styles -->
     <link rel="stylesheet" href="../css/style.css">
@@ -56,8 +57,7 @@ if ( !empty($_POST) ){
 <body class="bg-light">
     <!-- JUMBOTRON -->
     <div class="jumbotron bg-dark text-white text-center">
-        <h1 class="display-3">Cours PHP2022 - Entreprise et employes</h1>
-
+        <h1 class="display-3">Cours PHP7 - Entreprise et employés</h1>
     </div>
 
     <!-- RANGÉE PRINCIPALE -->
@@ -65,7 +65,6 @@ if ( !empty($_POST) ){
         <!-- LA NAVIGATION EN INCLUDE (penser à ajouter le JS qui va avec en fin de page) -->
         <?php
         require('../inc/sidenav.inc.php')
-
         ?>
 
         <!-- ============================================================== -->
@@ -73,63 +72,57 @@ if ( !empty($_POST) ){
         <!-- ============================================================== -->
         <div class="col-sm-8">
             <main class="container-fluid">
+                <!-- BOUTON DE LA NAV -->
+
 
                 <div class="row">
                     <hr>
-                    <h2 class="col-sm-12 text-center">1- Tableau des employes</h2>
+                    <h2 class="col-sm-12 text-center" id="">1 - Tableau des employés</h2>
                     <div class="col-sm-12">
                         <?php
-                            $requete = $pdoENT->query("SELECT * FROM employes ORDER BY prenom ");
+                        $requete = $pdoENT->query("SELECT * FROM employes ORDER BY prenom");
 
-                            $nbr_employes = $requete->rowCount();
+                        $nbr_employes = $requete->rowCount();
 
-                            echo "<p>Il y a " .$nbr_employes. " employés dans la BDD.</p>";
+                        echo "<p>Il y a " . $nbr_employes . " employés dans la base.</p>";
 
-                            echo "<table class=\"table table-dark table-striped\">";
-                                echo "<thead><tr>
-                                <th scope = \"col\">ID</th>
-                                <th scope = \"col\">Civilité</th>
-                                <th scope = \"col\">Prénom</th>
-                                <th scope = \"col\">Nom</th>
-                                <th scope = \"col\">Sexe</th>
-                                <th scope = \"col\">Service</th>
-                                <th scope = \"col\">Date d'embauche</th>
-                                <th scope = \"col\">Salaire</th>
-                                <th scope = \"col\">Fiche</th>
-                                </tr></thead>";
+                        echo "<table p-5 class=\"table table-light table-hover table-striped\">";
+                        echo "<thead><th scope=\"col\">ID</th><th scope=\"col\">Prénom</th><th scope=\"col\">Nom</th><th scope=\"col\">Sexe</th><th scope=\"col\">Service</th><th scope=\"col\">Date d'embauche</th><th scope=\"col\">Salaire</th><th scope=\"col\">Fiche</th></tr></thead>";
+                        while ($ligne = $requete->fetch(PDO::FETCH_ASSOC)) {
 
-                                while ($ligne = $requete->fetch(PDO::FETCH_ASSOC)) {
-                                    echo "<tr>";
-                                    echo "<td>#" . $ligne['id_employes'] . "</td>";
-                                    echo "<td>";
-                                    if ($ligne['sexe'] =='f') {
-                                        echo "Mme";
-                                    }else{                                
-                                        echo "M.";
-                                    }
-                                    "</td>";
-                                    echo "<td>" . $ligne['prenom'] . "</td>";
-                                    echo "<td>" . $ligne['nom'] . "</td>";
-                                    echo "<td>" . $ligne['sexe'] . "</td>";
-                                    echo "<td>" . $ligne['service'] . "</td>";
-                                    echo "<td>" .date('d/m/Y', strtotime($ligne['date_embauche']))  . "</td>";
-                                    echo "<td>" .number_format($ligne['salaire'])  . "€</td>";
-                                    echo "<td><a href=\"03-fiche-employes.php?id_employes=".$ligne['id_employes']. "\"class=\"text-white\">Voir sa fiche</a></td>";
+                            echo "<tr>";
+                            echo "<td>#" . $ligne['id_employes'] . "</td>";
+                            echo "<td>";
+                            if ($ligne['sexe'] == 'f') {
+                                echo "Mme ";
+                            } else {
+                                echo "M. ";
+                            }
+                            echo $ligne['prenom'] . "</td>";
+                            echo "<td>" . $ligne['nom'] . "</td>";
+                            echo "<td>" . $ligne['sexe'] . "</td>";
+                            echo "<td>" . $ligne['service'] . "</td>";
+                            echo "<td>" . date('d/m/Y', strtotime($ligne['date_embauche'])) . "</td>";
+                            echo "<td>" . number_format($ligne['salaire']) . " €</td>";
+                            echo "<td><a href=\"03-fiche-employes.php?id_employes=" . $ligne['id_employes'] . "\" class=\"text-white\">Voir sa fiche</a></td>";
 
+                            echo "</tr>";
+                        }
 
-                                    echo "</tr>";
-                                }
-
-                            echo "</table>";
+                        echo "</table>";
                         ?>
 
-                    </div>
-                </div><!-- fin de la rangée (row)-->
+
+                    </div><!-- fin de la colonne -->
+
+                </div><!-- fin de la rangée -->
+
 
                 <br><br>
 
             </main>
         </div> <!-- FIN DE LA PARTIE PRINCIPALE COL-8 -->
+
 
     </div>
 
@@ -140,9 +133,7 @@ if ( !empty($_POST) ){
 
     <!-- Optional JavaScript -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
     <!-- le js pour la navigation  -->
